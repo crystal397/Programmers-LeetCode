b56cb368cae5b5c531a069022fbdc552055091a5
Crystal <85814744+crystal397@users.noreply.github.com>
2024-07-10 10:38:33 +0900

    Update 조건에 맞는 도서 리스트 출력하기.sql
    update explanation

--- MySQL/프로그래머스/1/144853. 조건에 맞는 도서 리스트 출력하기/조건에 맞는 도서 리스트 출력하기.sql
+++ MySQL/프로그래머스/1/144853. 조건에 맞는 도서 리스트 출력하기/조건에 맞는 도서 리스트 출력하기.sql
@@ -1,4 +1,6 @@
--- 코드를 입력하세요
+-- DATE format을 YYYY-MM-DD 형식으로의 변환을 위해, date_format(DATE, '%Y-%m-%d')를 사용하였으며 
+--              YYYY만 추출해서 값 비교를 위해, YEAR(DATE)를 사용하였습니다.
+
 SELECT BOOK_ID, date_format(PUBLISHED_DATE, '%Y-%m-%d') AS PUBLISHED_DATE
 FROM BOOK
 WHERE YEAR(PUBLISHED_DATE) = '2021' 
