51bd0a087656b107b6a66e4b213d281a16262dc0
Crystal <85814744+crystal397@users.noreply.github.com>
2024-07-10 12:42:52 +0900

    Update 재구매가 일어난 상품과 회원 리스트 구하기.sql
    update explanation

--- MySQL/프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ MySQL/프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -1,4 +1,7 @@
--- 코드를 입력하세요
+-- 중복값을 찾기 위해 
+-- 먼저, GROUP BY로 필요한 조건을 묶고
+-- 묶은 그룹 중 중복이 1보다 큰 경우(= 중복되었다고 간주)를 출력하였습니다.
+
 SELECT USER_ID, PRODUCT_ID
 FROM ONLINE_SALE
 GROUP BY USER_ID, PRODUCT_ID
