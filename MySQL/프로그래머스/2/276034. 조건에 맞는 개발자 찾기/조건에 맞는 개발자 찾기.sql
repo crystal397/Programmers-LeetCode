8b8ee97be32aff52b38b1087cfdb530b14699790
Crystal <85814744+crystal397@users.noreply.github.com>
2024-07-10 10:42:33 +0900

    Update 조건에 맞는 개발자 찾기.sql
    update explanation

--- MySQL/프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ MySQL/프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,4 +1,6 @@
--- 코드를 작성해주세요
+-- 직관적으로, 비트연산 &를 이용해서 파이썬을 사용하거나 C#을 사용하는 사람인지 파악하였습니다.
+-- 조인 또는 with 문을 사용하여 추후 업데이트 예정입니다.
+
 SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
 FROM DEVELOPERS
 WHERE (SKILL_CODE & (SELECT CODE
