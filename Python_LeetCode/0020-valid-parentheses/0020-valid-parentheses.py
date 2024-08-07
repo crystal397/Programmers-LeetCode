24e07f17a84087c23c10bd013733ef93869b80f9
Crystal <85814744+crystal397@users.noreply.github.com>
2024-08-07 17:47:44 +0900

    Time: 36 ms (52.5%), Space: 16.7 MB (11.22%) - LeetHub

--- Python_LeetCode/0020-valid-parentheses/0020-valid-parentheses.py
+++ Python_LeetCode/0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,19 +1,24 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        q = []
+        # confirm the pair
+        counter = Counter(list(s))
+        if counter['('] != counter[')'] or counter['['] != counter[']'] or counter['{'] != counter['}']:
+            return False
+
+        stack = []
         start = ['(', '[', '{']
         end =[')', ']', '}']
         dict = {')':'(', ']':'[', '}':'{'}
 
         for i in s:
             if i in start:
-                q.append(i)
-            elif i in end and dict[i] in q and dict[i] == q.pop():
+                stack.append(i)
+            elif i in end and dict[i] in stack and dict[i] == stack.pop():
                 continue
             else:
                 return False
         
-        if len(q) != 0:
+        if len(stack) != 0:
             return False
 
         return True
