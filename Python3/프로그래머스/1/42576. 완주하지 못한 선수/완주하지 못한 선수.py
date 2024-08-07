4d655518f33bdfc806a3920d2bd95fc0027b88cf
Crystal <85814744+crystal397@users.noreply.github.com>
2024-08-07 11:07:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.29 ms, Memory: 39 MB -BaekjoonHub

--- Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,15 +1,10 @@
-def solution(participant, completion):
-    answer = ''
-    info_player = {}
-    for i in participant:
-        if i in info_player.keys():
-            info_player[i] += 1
-        else:
-            info_player[i] = 1
-    for i in completion:
-        if i in info_player.keys():
-            info_player[i] -= 1
-            if info_player[i] == 0:
-                del info_player[i]
-    answer = list(info_player.keys())
-    return answer[0]
+from collections import Counter
+
+def solution(participant, completion):    
+    counter_p = Counter(participant)
+    counter_c = Counter(completion)
+    
+    common_diff = counter_p - counter_c
+    
+    for key in common_diff.keys():
+        return key
