0e7e6ae3c7b75b3cfeff8c322219de4fdbe11cd2
Crystal <85814744+crystal397@users.noreply.github.com>
2024-08-07 11:23:03 +0900

    [level 2] Title: 올바른 괄호, Time: 11.69 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/12909. 올바른 괄호/README.md
+++ Python3/프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 7.15 ms
+메모리: 10.3 MB, 시간: 11.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 25일 18:14:27
+2024년 08월 07일 11:23:00
 
 ### 문제 설명
 
--- Python3/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ Python3/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -2,21 +2,19 @@ from collections import deque
 
 def solution(s):
     answer = True
-
-    # deque 선언
     q = deque()
+    
     for i in s:
         if i == "(":
             q.append(i)
         else:
-            if '(' not in q:
+            if (len(q) == 0) or ("(" not in q):
                 return False
             else:
-                q.pop()
+                q.popleft()
+                
     if len(q) != 0:
         return False
-            
-            
     # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
     print('Hello Python')
 
