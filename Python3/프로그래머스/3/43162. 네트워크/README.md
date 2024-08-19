bf00118eb654173039f0fc62f9d64cb9e408a429
Crystal <85814744+crystal397@users.noreply.github.com>
2024-08-19 14:36:48 +0900

    [level 3] Title: 네트워크, Time: 0.51 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/3/43162. 네트워크/README.md
+++ Python3/프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.54 ms
+메모리: 10.3 MB, 시간: 0.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 16일 15:03:48
+2024년 08월 19일 14:36:42
 
 ### 문제 설명
 
--- Python3/프로그래머스/3/43162. 네트워크/네트워크.py
+++ Python3/프로그래머스/3/43162. 네트워크/네트워크.py
@@ -2,35 +2,25 @@ from collections import deque
 
 def solution(n, computers):
     answer = 0
-    a_list = [[] for _ in range(n)]
-
-    for i in range(n):
-        for j in range(n):
-            if i != j and computers[i][j] == 1:
-                a_list[i].append(j)
-    
-    def bfs(a_list, start_v):
-        # initial settings
-        q = deque()
-        visited = [False] * len(a_list)
-    
-        # first
-        q.append(start_v)
-        visited[start_v] = True
-
-        # loop
-        while q:
-            cur_v = q.popleft()
-            for next_v in a_list[cur_v]:
-                if visited[next_v] == False:
-                    q.append(next_v)
-                    visited[next_v] = True
-                    
-        answer += 1
-        
-        return answer
+    visited = [False] * n
     
     for start_v in range(n):
-        bfs(a_list, start_v)
-            
+        if visited[start_v] == False:
+            bfs(computers, start_v, visited)
+            answer += 1
+                
     return answer
+
+def bfs(computers, start_v, visited):
+    q = deque()
+    
+    q.append(start_v)
+    visited[start_v] = True
+    
+    while q:
+        cur_v = q.popleft()
+        for next_v, num in enumerate(computers[cur_v]):
+            if visited[next_v] == False and num == 1:
+                q.append(next_v)
+                visited[next_v] = True
+    return visited
