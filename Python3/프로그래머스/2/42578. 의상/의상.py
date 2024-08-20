3636b2591dff5000f1bed2e751504f143620b61c
Crystal <85814744+crystal397@users.noreply.github.com>
2024-08-20 16:14:19 +0900

    Update 의상.py

--- Python3/프로그래머스/2/42578. 의상/의상.py
+++ Python3/프로그래머스/2/42578. 의상/의상.py
@@ -3,7 +3,7 @@ import math
 
 def solution(clothes):
     answer = 0
-    v = Counter([c[1] for c in clothes])
+    v = Counter([c[1] for c in clothes]) # 같은 종류의 카테고리끼리 묶기 + 묶은 카테고리의 종류는 각각 몇 개인지 파악
     k = list(v.values())
-    answer = math.prod([i+1 for i in k]) - 1
+    answer = math.prod([i+1 for i in k]) - 1 # 리스트 내 모든 값 곱하기
     return answer
