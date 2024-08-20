b08b007b33321be7c8ee37044d009af13b3d926c
Crystal <85814744+crystal397@users.noreply.github.com>
2024-08-20 18:32:57 +0900

    Time: 36 ms (51.44%), Space: 16.6 MB (48.17%) - LeetHub

--- Python_LeetCode/0020-valid-parentheses/0020-valid-parentheses.py
+++ Python_LeetCode/0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,26 +1,15 @@
-from collections import Counter
-
 class Solution:
     def isValid(self, s: str) -> bool:
-        # confirm the pair first
-        counter = Counter(s)
-        if counter['('] != counter[')'] or counter['['] != counter[']'] or counter['{'] != counter['}']:
-            return False
-
         stack = []
-        dict = {')':'(', ']':'[', '}':'{'}
-
-        for i in s:
-            if dict.get(i) == None:
-                stack.append(i)
-            elif len(stack) != 0 and dict[i] == stack.pop():
-                continue
+        dic = {')':'(', ']':'[', '}':'{'}
+        for p in s:
+            if p in dic.values():
+                stack.append(p)
             else:
-                return False
-        
+                if len(stack) != 0 and stack[-1] == dic[p]:
+                    stack.pop()
+                else:
+                    return False
         if len(stack) != 0:
             return False
-
         return True
-
-
