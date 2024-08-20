1a7ed50ac839c82ff0017a5d5173c31c171baa5a
Crystal <85814744+crystal397@users.noreply.github.com>
2024-08-20 18:45:26 +0900

    [level 2] Title: 괄호 회전하기, Time: 123.79 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ Python3/프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,35 +1,23 @@
-from collections import deque, Counter
-
-def confirm(s):
-    stack = []
-    dic = {']':'[', '}':'{', ')':'('}
-
-    # first confirm
-    counter = Counter(s)
-    if counter['('] != counter[')'] or counter['['] != counter[']'] or counter['{'] != counter['}']:
-        return 0
-
-    # second confirm
-    for p in s:
-        if dic.get(p) == None:
-            stack.append(p)
-        elif len(stack) != 0 and stack.pop() == dic[p]:
-            continue
-        else:
-            return 0
-
-    # last confirm
-    if len(stack) != 0:
-        return 0
-    
-    return 1
-    
-
-def solution(s):
+def solution(ss):
     answer = 0
-    q = deque(s)
-    for i in range(len(s)):
-        answer += confirm(''.join([str(item) for item in q]))
-        p = q.popleft()
-        q.append(p)
+    
+    def confirm(s):
+        stack = []
+        m = {')':'(',']':'[','}':'{'}
+        for p in s:
+            if p in m.values():
+                stack.append(p)
+            else:
+                if len(stack) != 0 and stack.pop() == m[p]:
+                    continue
+                else:
+                    return 0
+        if len(stack) != 0:
+            return 0
+        return 1
+    
+    for i in range(len(ss)):
+        s = ss[i:] + ss[:i]
+        answer += confirm(s)
+    
     return answer
