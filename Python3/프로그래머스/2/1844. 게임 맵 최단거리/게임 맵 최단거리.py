156e73efc5b49d45a6df6cabf31ec2dac137c6a3
Crystal <85814744+crystal397@users.noreply.github.com>
2024-08-21 13:19:20 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.76 ms, Memory: 10.4 MB -BaekjoonHub

--- Python3/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ Python3/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,29 +1,32 @@
 from collections import deque
 
 def solution(maps):
+    answer = 0
+
+    q = deque()
     row = len(maps)
     col = len(maps[0])
     dr = [1, 0, -1, 0]
     dc = [0, 1, 0, -1]
-    
-    q = deque()
     visited = [[False] * col for _ in range(row)]
     
-    sr, sc, dst = (0,0,1)
-    q.append((sr, sc, dst))
-    visited[sr][sc] == True
+    q.append((0,0,1))
+    visited[0][0] = True
     
     while q:
         cr, cc, dst = q.popleft()
-        if cr == row -1 and cc == col -1:
+        if (cr, cc) == (row-1, col-1):
             return dst
+        else:
+            for i in range(len(dr)):
+                nr = cr + dr[i]
+                nc = cc + dc[i]
+                if 0 <= nr < row and 0 <= nc < col and maps[nr][nc] == 1:
+                    if visited[nr][nc] == False:
+                        q.append((nr, nc, dst+1))
+                        visited[nr][nc] = True
 
-        for i in range(len(dr)):
-            nr = cr + dr[i]
-            nc = cc + dc[i]
-            if 0 <= nr < row and 0 <= nc < col and maps[nr][nc] == 1:
-                if visited[nr][nc] == False:
-                    q.append((nr,nc,dst+1))
-                    visited[nr][nc] = True
+    if visited[row-1][col-1] == False:
+        return -1
     
-    return -1
+    return answer
