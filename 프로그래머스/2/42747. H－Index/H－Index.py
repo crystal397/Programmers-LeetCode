55e1bbea2705bfe97fcf074e3ca97d977cc637b0
Crystal <85814744+crystal397@users.noreply.github.com>
2024-09-24 22:28:55 +0900

    [level 2] Title: H-Index, Time: 10.21 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,7 +1,6 @@
 def solution(citations):
     l = len(citations)
-    if set(citations) == {0}:
-        return 0
     for i in range(l, 0, -1):
         if len(list(filter(lambda x : x >= i, citations))) >= i:
             return i
+    return 0
