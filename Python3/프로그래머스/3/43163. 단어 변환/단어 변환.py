3800b9ed1f2cc1368d4c74d14916b8c9c5023f15
Crystal <85814744+crystal397@users.noreply.github.com>
2024-10-14 11:26:22 +0900

    Update 단어 변환.py

--- Python3/프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ Python3/프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,3 +1,27 @@
+from collections import deque
+
 def solution(begin, target, words):
+    
+    if not target in words:
+        return 0
+    
     answer = 0
+    v = [0] * len(words)
+    q = deque()
+    q.append([begin, 0])
+
+    while q:
+        word, cnt = q.popleft()
+        if word == target:
+            return cnt
+        for i in range(len(words)):
+            tmp_cnt = 0
+            if not v[i]:
+                for j in range(len(word)):
+                    if word[j] != words[i][j]:
+                        tmp_cnt += 1
+                if tmp_cnt == 1:
+                    q.append([words[i], cnt+1])
+                    v[i] = 1
+                        
     return answer
