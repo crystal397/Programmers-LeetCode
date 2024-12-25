f45b408398e82700794a9ca7ce59ed37ea8b770b
Crystal <85814744+crystal397@users.noreply.github.com>
2024-12-25 21:34:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.28 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,11 +2,9 @@ from collections import Counter
 
 def solution(participant, completion):
 
-    counter_p = Counter(participant)
-    counter_c = Counter(completion)
-    result = counter_p - counter_c
-
-    result_p = [key for key in counter_p.keys() if key in result]
-
-    return result_p[0]
+    p = Counter(participant)
+    c = Counter(completion)
+    i = p - c
+    for key in i.keys():
+        return key
 
