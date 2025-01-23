68ae05d764130e6021bb6260047295c7e58f92f6
Crystal <85814744+crystal397@users.noreply.github.com>
2025-01-23 16:54:50 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -4,12 +4,6 @@ for i in range(28):
     n = int(input())
     arr[n] = 1
 
-result = []
-first_index = arr.index(0, 1)
-result.append(first_index)
-next_index = first_index+1
-next_index = arr.index(0, next_index)
-result.append(next_index)
-
-for i in range(len(result)):
-    print(result[i])
+for j in range(1, 31):
+    if arr[j] == 0:
+        print(j)
